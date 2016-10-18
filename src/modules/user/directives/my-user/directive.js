79a046796a19905b0e9427fa319ad0f3d75534f4
Kiwouz <espritkiwi@gmail.com>
2016-10-18 15:59:26 +0200

    Fix TD3

--- src/modules/user/directives/my-user/directive.js
+++ src/modules/user/directives/my-user/directive.js
@@ -39,7 +39,7 @@ angular.module('eklabs.angularStarterPack.user')
                 scope.isModeEdition = false;
                 scope.goToEdition = function(){
                     scope.isModeEdition = !scope.isModeEdition;
-                    scope.userEdit = scope.user;
+                    scope.userEdit = angular.extend({},scope.user);
                 };
 
                 /**

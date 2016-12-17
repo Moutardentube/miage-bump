a7ff8b5e50f59327c5ed5b676f8ba534dd8e8981
Ludovic <moutardentube@free.fr>
2016-12-17 19:57:45 +0100

    Fix merge

--- files.js
+++ files.js
@@ -9,17 +9,17 @@ modelFiles = {
 
         'src/modules/form-editor/module.js',
         'src/modules/form-editor/{,**/}*.js',
-
-        'src/modules/bump-button/module.js',
-        'src/modules/bump-button/{,**/}*.js',
-        'src/modules/bump-profile/module.js',
-        'src/modules/bump-profile/{,**/}*.js',
         
         'src/modules/forms/module.js',
         'src/modules/forms/{,**/}*.js',
 
         'src/modules/user/module.js',
         'src/modules/user/{,**/}*.js',
+
+        'src/modules/bump-button/module.js',
+        'src/modules/bump-button/{,**/}*.js',
+        'src/modules/bump-profile/module.js',
+        'src/modules/bump-profile/{,**/}*.js',
 
         'src/module.js',
         'src/filters/{,**/}*.js'

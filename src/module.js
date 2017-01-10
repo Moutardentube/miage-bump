fe51f7668fba0c686caf45a18f4a84824359060f
Ludovic <moutardentube@free.fr>
2017-01-10 15:12:29 +0100

    Ajout des icônes material design dans la directive

--- src/module.js
+++ src/module.js
@@ -1,7 +1,10 @@
 'use strict';
 
-angular.module('miage.bump',[
+angular.module('miage.bump', [
     'miage.bump.button',
     'miage.bump.profile',
-    'eklabs.angularStarterPack.user'
-]);
+    'eklabs.angularStarterPack.user',
+    'ngMaterial'
+]).config(function ($mdIconProvider) {
+    $mdIconProvider.iconSet('material-design', '/public/material-design.svg', 24);
+});
